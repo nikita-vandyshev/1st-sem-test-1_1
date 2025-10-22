1692dac97c2dbf675a3512bf0bf3890dca8cc6b9
Efim Kubyshkin <efimkub@mail.ru>
2025-10-23 00:32:37 +0300

    Add Luhn algorithm

---
+++ src/luhn.py
@@ -0,0 +1,14 @@
+def luhnСheck(cardNumber):
+    digits = [int(d) for d in str(cardNumber) if d.isdigit()]
+    control = digits.pop()
+    parity = (len(digits))%2
+    total = 0
+    for i in range(len(digits)):
+        if i % 2 == parity:
+            doubled = digits[i] * 2
+            if doubled > 9:
+                doubled -= 9
+            total += doubled
+        else:
+            total += digits[i]
+    return (total + control) % 10 == 0
