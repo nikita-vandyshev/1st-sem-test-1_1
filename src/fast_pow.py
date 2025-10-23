5efab5ec9d8972492bb2b73abc3eda5188c10a6d
nikita-vandyshev <nikvandyshev@gmail.com>
2025-10-23 15:07:06 +0300

    fix commit
    Исправления:
    Теперь код возводит нужную степень
    Теперь алгоритм завершается при power == 0
    Теперь цикл не становится бесконечным при power < 0
    Проверяются нецелые степени

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,6 +1,15 @@
 def fastPow(number, power):
-    result = number
-    while power != 1:
-        result *= result
+    if not isinstance(power, int):
+        raise ValueError("Power must be integer")
+    if power < 0:
+        raise ValueError("Power must be non-negative")
+    if power == 0:
+        return 1 
+    result = 1
+    base = number
+    while power > 0:
+        if power % 2 == 1:
+            result *= base
+        base *= base
         power = power // 2
     return result
